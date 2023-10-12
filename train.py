472f7db8cc468d37ba5da43a5a22e9050e5b12b3
foxyhyun <86210630+foxyhyun@users.noreply.github.com>
2023-10-12 09:21:03 +0900

    Add files via upload

--- train.py
+++ train.py
@@ -1,21 +1,14 @@
 import os
 import zipfile
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
-import cv2
+from PIL import Image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
-
-def unzip_data(zip_filename, target_dir):
-    with zipfile.ZipFile(zip_filename, 'r') as zip_ref:
-        zip_ref.extractall(target_dir)
-
-# 'data' 폴더에 압축 해제
-unzip_data('data/phone.zip', 'data/phone')
-unzip_data('data/sleeping.zip', 'data/sleeping')
-unzip_data('data/stepOut.zip', 'data/stepOut')
-unzip_data('data/studying.zip', 'data/studying')
+from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from keras.optimizers import SGD
 
 # Image Generator
 datagen = ImageDataGenerator(
@@ -43,53 +36,71 @@ validation_generator = datagen.flow_from_directory(
     class_mode='categorical',
     subset='validation'
 )
+print(train_generator.num_classes)
+# MobileNet 모델을 불러옵니다. include_top=False로 설정하여 분류 레이어를 포함하지 않습니다.
+base_model = MobileNet(weights='imagenet', include_top=False)
 
-import matplotlib.pyplot as plt
-import numpy as np
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
+# 새로운 분류 레이어를 추가합니다.
+x = base_model.output
+x = GlobalAveragePooling2D()(x)  # 특성 맵을 평균화합니다.
+x = Dense(1024, activation='relu')(x)  # 완전 연결 레이어를 추가합니다.
+predictions = Dense(train_generator.num_classes, activation='softmax')(x)  # 출력 레이어를 추가합니다.
 
-# 원본 이미지 불러오기
-img = load_img('data/studying/img1.jpg')  # 해당 경로에 원하는 이미지를 넣으세요
-x = img_to_array(img)
-x = x.reshape((1,) + x.shape)
+# 새로운 모델을 정의합니다.
+model = Model(inputs=base_model.input, outputs=predictions)
 
-# 증강 방법별 ImageDataGenerator 생성
-augmentations = {
-    'Original': ImageDataGenerator(rescale=1./255),
-    'Rotation': ImageDataGenerator(rescale=1./255, rotation_range=40),
-    'Width Shift': ImageDataGenerator(rescale=1./255, width_shift_range=0.2),
-    'Height Shift': ImageDataGenerator(rescale=1./255, height_shift_range=0.2),
-    'Horizontal Flip': ImageDataGenerator(rescale=1./255, horizontal_flip=True),
-}
+# 미리 훈련된 가중치를 사용한 MobileNet 모델의 일부를 동결합니다.
+for layer in base_model.layers:
+    layer.trainable = False
 
-# 이미지 시각화
-fig, axes = plt.subplots(1, len(augmentations), figsize=(20, 20))
+# 모델을 컴파일합니다.
+model.compile(
+    optimizer= SGD(lr=0.001, momentum=0.9), 
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+    )
 
-for ax, (name, aug) in zip(axes, augmentations.items()):
-    aug_img = next(aug.flow(x, batch_size=1))
-    ax.imshow(aug_img[0])
-    ax.set_title(name)
-    ax.axis('off')
+history = model.fit(train_generator, validation_data=validation_generator, epochs=100)
 
+# 훈련과 검증 손실을 시각화
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.legend()
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
 plt.show()
 
-# model = Sequential()
-# model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)))
-# model.add(MaxPooling2D((2, 2)))
-# model.add(Conv2D(64, (3, 3), activation='relu'))
-# model.add(MaxPooling2D((2, 2)))
-# model.add(Conv2D(128, (3, 3), activation='relu'))
-# model.add(MaxPooling2D((2, 2)))
-# model.add(Flatten())
-# model.add(Dense(128, activation='relu'))
-# model.add(Dense(4, activation='softmax'))  # 4개의 클래스
+# 모델을 저장합니다.
+model.save('mobile_net_model.h5')
 
-# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# # 저장한 모델 파일의 경로 설정
+# model_path = 'mobile_net_model.h5'
 
-# history = model.fit(
-#     train_generator,
-#     steps_per_epoch=len(train_generator),
-#     epochs=10,
-#     validation_data=validation_generator,
-#     validation_steps=len(validation_generator)
-# )
+# # 모델 로드
+# loaded_model = tf.keras.models.load_model(model_path)
+
+
+# 테스트 데이터 제너레이터 생성
+test_datagen = ImageDataGenerator(rescale=1./255)  # 데이터 스케일링만 수행합니다.
+
+test_generator = test_datagen.flow_from_directory(
+    'test',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical',  # 다중 클래스 분류일 경우 'categorical'로 설정
+    shuffle=False  # 테스트 데이터는 셔플하지 않습니다.
+)
+
+# 모델 평가
+test_loss, test_accuracy = model.evaluate(test_generator)
+
+print(f'Test Loss: {test_loss:.4f}')
+print(f'Test Accuracy: {test_accuracy:.4f}')
+
+# 테스트 데이터에 대한 예측 수행
+predictions = model.predict(test_generator)
+
+# 예측 결과 확인
+print("Predictions:")
+print(predictions)
+
